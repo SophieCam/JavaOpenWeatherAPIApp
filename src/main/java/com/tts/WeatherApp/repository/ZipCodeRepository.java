f9c81acb37b139921bc951a9bfc4ad9b20fc7e7f
SophieCam <79154836+SophieCam@users.noreply.github.com>
2021-05-28 16:15:55 -0400

    weather app

---
+++ src/main/java/com/tts/WeatherApp/repository/ZipCodeRepository.java
@@ -0,0 +1,8 @@
+package com.tts.WeatherApp.repository;
+
+import com.tts.WeatherApp.model.ZipCode;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ZipCodeRepository extends CrudRepository<ZipCode, Long> {
+
+}
